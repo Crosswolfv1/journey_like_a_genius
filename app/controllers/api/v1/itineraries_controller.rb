ee69597b6d712aff1af5c641c4eb30ebe58da314
James-Cochran <jameslikesfruit@gmail.com>
2025-01-14 16:35:52 -0500

    added forgery and authenticity token skip to itineraries controller.

--- app/controllers/api/v1/itineraries_controller.rb
+++ app/controllers/api/v1/itineraries_controller.rb
@@ -1,14 +1,19 @@
 class Api::V1::ItinerariesController < ApplicationController
+  protect_from_forgery with: :null_session
+  skip_before_action :verify_authenticity_token
+
   def index
     render json: ItinerarySerializer.format_itinerary_list(Itinerary.all), status: :ok
   end
 
   def create
-    binding.pry
+    # binding.pry
+    params[:itinerary][:user_id] = params[:itinerary][:user_id].to_i
     itinerary = Itinerary.new(itinerary_params)
     if itinerary.save
       render json: ItinerarySerializer.format_itinerary_list([itinerary]), status: :ok
     else
+      render json: { errors: itinerary.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
