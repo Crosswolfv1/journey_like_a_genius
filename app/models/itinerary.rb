6e6f4c29ce5c672388b9b2a6a0e5e3a34fc7ca03
James-Cochran <jameslikesfruit@gmail.com>
2025-01-15 13:14:31 -0500

    added itinerary model method to add items to an itinerary, removed that functionality from the controller.

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -4,4 +4,17 @@ class Itinerary < ApplicationRecord
   has_many :items, through: :item_itineraries
   validates :city, presence: true
   validates :duration, presence: true
+
+  def add_items(items_data)
+    items_data.each do | item_data |
+      item = Item.find_or_create_by(name: item_data[:name], address: item_data[:address]) do | new_item |
+        new_item.item_type = item_data[:item_type]
+        new_item.phone = item_data[:phone]
+        new_item.opening_hours = item_data[:opening_hours]
+      end
+      if item.valid? && item.save
+        self.items << item
+      end
+    end
+  end
 end
