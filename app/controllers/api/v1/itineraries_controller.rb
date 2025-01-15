e5c64bfac70ec651e72e9942fefa284b1705724f
James-Cochran <jameslikesfruit@gmail.com>
2025-01-14 19:04:35 -0500

    added itinerary create controller functionality to get items associated with itineraries.

--- app/controllers/api/v1/itineraries_controller.rb
+++ app/controllers/api/v1/itineraries_controller.rb
@@ -10,6 +10,16 @@ class Api::V1::ItinerariesController < ApplicationController
     params[:itinerary][:user_id] = params[:itinerary][:user_id].to_i
     itinerary = Itinerary.new(itinerary_params)
     if itinerary.save
+      if params[:itinerary][:items].each do | item_data |
+        item = Item.find_or_create_by(name: item_data[:name], address: item_data[:address]) do | new_item |
+          new_item.item_type = item_data[:item_type]
+          new_item.phone = item_data[:phone]
+          new_item.opening_hours = item_data[:opening_hours]
+
+        end
+        itinerary.items << item 
+      end
+    end
       render json: ItinerarySerializer.format_itinerary_list([itinerary]), status: :ok
     else
       render json: { errors: itinerary.errors.full_messages }, status: :unprocessable_entity
@@ -19,5 +29,9 @@ class Api::V1::ItinerariesController < ApplicationController
   private
   def itinerary_params
     params.require(:itinerary).permit(:city, :duration, :user_id)
+  end
+
+  def item_params(item_data)
+    item_data.permit(:name, :address, :item_type, :phone, opening_hours: [])
   end
 end
