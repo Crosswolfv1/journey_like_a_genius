01affc89cfc1ff84735972951ff41ca02a45cbd7
Joel Davalos <jsdavalos@gmail.com>
2025-01-15 17:26:17 -0700

    Refactor: updated User index.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::UsersController < ApplicationController
   def index
-    render json: UserSerializer.format_user_list(User.all), status: :ok
+    user = User.find_by(id: params[:id])
+    if user
+      render json: UserSerializer.format_user_list(User.all), status: :ok
+    else 
+      render json: { error: "User not found" }, status: :not_found
+    end
   end
 end
