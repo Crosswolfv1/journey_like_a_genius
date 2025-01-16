a2b96acf6ae479543194a4ab173b7d812318f0fd
Joel Davalos <jsdavalos@gmail.com>
2025-01-16 15:35:46 -0700

    Feat: Added actions for finding one and all users.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,12 @@
 class Api::V1::UsersController < ApplicationController
   def index
+      render json: UserSerializer.format_user_list(User.all), status: :ok
+  end
+
+  def show
     user = User.find_by(id: params[:id])
     if user
-      render json: UserSerializer.format_user_list(User.all), status: :ok
+      render json: UserSerializer.format_user(user), status: :ok
     else 
       render json: { error: "User not found" }, status: :not_found
     end
