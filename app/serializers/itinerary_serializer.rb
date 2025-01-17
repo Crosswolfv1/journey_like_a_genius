9a4f8c55d70dfdb97651f88a5ac0b9bb491f8588
Jeremiah Ross <crosswolf.20work@gmail.com>
2025-01-16 18:13:53 -0600

    Feat created new method to serialize a single itinerary after creation

--- app/serializers/itinerary_serializer.rb
+++ app/serializers/itinerary_serializer.rb
@@ -1,6 +1,6 @@
 class ItinerarySerializer
 
-  def self.format_itinerary_list(itineraries)
+  def self.format_itinerary_lists(itineraries)
     { data:
         itineraries.map do |itinerary|
           {
@@ -27,5 +27,28 @@ class ItinerarySerializer
           your_itineraries: itineraries.count
         }
     }
+  end
+
+  def self.format_itinerary_list(itinerary)
+    {data:
+    {
+      id: itinerary.id.to_s,
+      type: "itinerary",
+      attributes: {
+        city: itinerary.city,
+        duration: itinerary.duration,
+        user_id: itinerary.user.id,
+        items: itinerary.items.map do |item|
+           {
+            id: item.id.to_s,
+            item_type: item.item_type,
+            name: item.name,
+            address: item.address,
+            opening_hours: item.opening_hours,
+            phone: item.phone
+           }
+          end
+      }
+    }}
   end
 end
