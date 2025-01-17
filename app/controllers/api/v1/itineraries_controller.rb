9684715387dca292b3a1fd9510c6c8dbc3f70fd7
Jeremiah Ross <crosswolf.20work@gmail.com>
2025-01-17 09:49:56 -0600

    cleaned up methods

--- app/controllers/api/v1/itineraries_controller.rb
+++ app/controllers/api/v1/itineraries_controller.rb
@@ -7,13 +7,12 @@ class Api::V1::ItinerariesController < ApplicationController
   end
 
   def create
+    user = User.find_by(id: params[:itinerary_user_id])
     itinerary = Itinerary.new(itinerary_params.except(:items))
-    itinerary.user_id = params[:itinerary_user_id].to_i
-    binding.pry
+    itinerary.user_id = user.id
     if itinerary.save
-      binding.pry
       itinerary.add_items(itinerary_params[:items]) if itinerary_params[:items].present? 
-      render json: ItinerarySerializer.format_itinerary_list([itinerary]), status: :ok
+      render json: ItinerarySerializer.format_itinerary_list(itinerary), status: :ok
     else
       render json: { errors: itinerary.errors.full_messages }, status: :unprocessable_entity 
     end
@@ -22,14 +21,6 @@ class Api::V1::ItinerariesController < ApplicationController
   private
   
   def itinerary_params
-    binding.pry
-    permitted_params = params.require(:itinerary).permit(:city, :duration, items: [:name, :address, :item_type, :phone, :opening_hours])
-    if permitted_params[:items].present?
-      permitted_params[:items] = permitted_params[:items].map do |item|
-        item.permit(:name, :address, :item_type, :phone, :opening_hours).to_h
-      end
-    end
-    binding.pry
-    permitted_params
+    params.require(:itinerary).permit(:city, :duration, items: [:name, :address, :item_type, :phone, :opening_hours])
   end
 end
