3a0b656cec2752c2d18fb21393ff8a57b6d5c61d
Jeremiah Ross <crosswolf.20work@gmail.com>
2025-01-17 09:50:44 -0600

    cleaned up methods

--- app/serializers/itinerary_serializer.rb
+++ app/serializers/itinerary_serializer.rb
@@ -30,25 +30,26 @@ class ItinerarySerializer
   end
 
   def self.format_itinerary_list(itinerary)
+    
     {data:
-    {
-      id: itinerary.id.to_s,
-      type: "itinerary",
-      attributes: {
-        city: itinerary.city,
-        duration: itinerary.duration,
-        user_id: itinerary.user.id,
-        items: itinerary.items.map do |item|
-           {
-            id: item.id.to_s,
-            item_type: item.item_type,
-            name: item.name,
-            address: item.address,
-            opening_hours: item.opening_hours,
-            phone: item.phone
-           }
-          end
-      }
+      {
+        id: itinerary.id,
+        type: "itinerary",
+        attributes: {
+          city: itinerary.city,
+          duration: itinerary.duration,
+          user_id: itinerary.user.id,
+          items: itinerary.items.map do |item|
+            {
+              id: item.id.to_s,
+              item_type: item.item_type,
+              name: item.name,
+              address: item.address,
+              opening_hours: item.opening_hours,
+              phone: item.phone
+            }
+            end
+        }
     }}
   end
 end
