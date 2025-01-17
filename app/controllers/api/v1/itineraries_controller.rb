b9ca06469cc1e11e71e7c319e4308a947aa71296
James-Cochran <jameslikesfruit@gmail.com>
2025-01-17 09:47:26 -0500

    still trying to convert the received json into ruby usable objects.

--- app/controllers/api/v1/itineraries_controller.rb
+++ app/controllers/api/v1/itineraries_controller.rb
@@ -8,7 +8,6 @@ class Api::V1::ItinerariesController < ApplicationController
 
   def create
     itinerary = Itinerary.new(itinerary_params.except(:items))
-    binding.pry
     itinerary.user_id = params[:itinerary_user_id].to_i
     binding.pry
     if itinerary.save
@@ -16,7 +15,6 @@ class Api::V1::ItinerariesController < ApplicationController
       itinerary.add_items(itinerary_params[:items]) if itinerary_params[:items].present? 
       render json: ItinerarySerializer.format_itinerary_list([itinerary]), status: :ok
     else
-      binding.pry
       render json: { errors: itinerary.errors.full_messages }, status: :unprocessable_entity 
     end
   end
@@ -24,8 +22,14 @@ class Api::V1::ItinerariesController < ApplicationController
   private
   
   def itinerary_params
+    binding.pry
     permitted_params = params.require(:itinerary).permit(:city, :duration, items: [:name, :address, :item_type, :phone, :opening_hours])
-    permitted_params[:items] = permitted_params[:items].map { |item| item.to_h } if permitted_params[:items].present?
+    if permitted_params[:items].present?
+      permitted_params[:items] = permitted_params[:items].map do |item|
+        item.permit(:name, :address, :item_type, :phone, :opening_hours).to_h
+      end
+    end
+    binding.pry
     permitted_params
   end
 end
