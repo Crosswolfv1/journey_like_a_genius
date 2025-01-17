dfaef201f596401465e356ef68c136dc1723f826
Jeremiah Ross <crosswolf.20work@gmail.com>
2025-01-17 09:50:24 -0600

    cleaned up methods

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -6,14 +6,7 @@ class Itinerary < ApplicationRecord
   validates :duration, presence: true
 
   def add_items(items_data)
-    if items_data.present?
-      items_data = items_data.map do |item|
-        item.permit(:name, :address, :item_type, :phone, :opening_hours).to_h
-      end
-    end
-    binding.pry
     items_data.each do | item_data |
-      binding.pry
       item = Item.find_or_create_by(name: item_data[:name], address: item_data[:address]) do | new_item |
         new_item.item_type = item_data[:item_type]
         new_item.phone = item_data[:phone]
