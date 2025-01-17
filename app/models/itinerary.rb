c7cc58a07a1259359c9affccfc71f18fb2da7b93
Jeremiah Ross <crosswolf.20work@gmail.com>
2025-01-17 14:26:13 -0600

    removed commenetd out line

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -10,7 +10,6 @@ class Itinerary < ApplicationRecord
       item = Item.find_or_create_by(name: item_data[:name], address: item_data[:address]) do | new_item |
         new_item.item_type = item_data[:item_type]
         new_item.phone = item_data[:phone]
-        # new_item.opening_hours = item_data[:opening_hours]
       end
       if item.valid? && item.save
         self.items << item
