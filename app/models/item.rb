e112621d4c012a3a98eb716b43c638ea07c3610b
Jeremiah Ross <crosswolf.20work@gmail.com>
2025-01-17 11:03:50 -0600

    commented out opening hours

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,6 @@ class Item < ApplicationRecord
   validates :item_type, presence: true
   validates :name, presence: true
   validates :address, presence: true
-  validates :opening_hours, presence: true
+  # validates :opening_hours, presence: true
   validates :phone, presence: true
 end
